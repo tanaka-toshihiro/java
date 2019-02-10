0304d120dbcce2d7a612827a947483fe9898590b
tanaka-toshihiro <46895756+tanaka-toshihiro@users.noreply.github.com>
2019-02-11 00:18:15 +0900

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,27 +5,26 @@ import java.util.Scanner;
 public class FizzBuzz {
 	public static void main(String[] args){
 	
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 	        
 	    	int x = scan.nextInt();      
 
  	         for(int i = 1; i <= x; i++){
  
- 	        	if(i%3==0){
+ 	            if(i%3==0){
+	             System.out.println("Fizz");
+		   
+		    }else if(i%5==0){
+	             System.out.println("Buzz");
+	             
+		    }else if(i%3==0 || i%5==0){
+	             System.out.println("FizzBuzz");
 	            
- 	        		System.out.println("Fizz");
-	            }else if(i%5==0){
-	            
-	            	System.out.println("Buzz");
-	             }else if(i%3==0 || i%5==0){
-	           
-	            	 System.out.println("FizzBuzz");
-	            }else{
-	           
-	            	System.out.println(i);
+		    }else{
+	             System.out.println(i);
 	            }
 	        
-            }    
+               }    
 
     }    
   
